afbd129c2b35c34aca1447fb16bf4506d9f32d2c
cwkimwider <cw.kim@widerplanet.com>
2022-07-14 23:05:59 +0900

    asdf

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("This is branch feature/TA-1234");
+        System.out.println("This is branch feature/TA-12345");
     }
 }
